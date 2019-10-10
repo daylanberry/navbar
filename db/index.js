224ae96845bc5e3fe9f841c7acdf3ba95e8d736e
SassySpaceMan <51337432+SassySpaceMan@users.noreply.github.com>
2019-10-10 11:02:37 -0700

    []

--- db/index.js
+++ db/index.js
@@ -2,8 +2,14 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
-var searchSchema = new mongoose.schema({
-  name: String,
-  type: String,
-  id: Number
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected');// we're connected!
 });
+
+// var searchSchema = new mongoose.schema({
+//   name: String,
+//   type: String,
+//   id: Number
+// });
